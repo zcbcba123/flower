6d42668a198c32d23a32e86ee424b1a812b497db
zcbcba123 <997027407@qq.com>
2019-01-22 21:03:25 +0800

    调整样式

--- app/src/main/java/com/love/flower/MainActivity.java
+++ app/src/main/java/com/love/flower/MainActivity.java
@@ -6,17 +6,17 @@ import android.os.Bundle;
 
 public class MainActivity extends BaseCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        //
-        setContentView(R.layout.activity_main);
-
-    }
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        //
+//        setContentView(R.layout.activity_main);
+//
+//    }
 
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_main;
     }
 
     @Override
